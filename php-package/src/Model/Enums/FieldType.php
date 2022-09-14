ab52ac0dcf2121f989b2ec73025dd1dd0cee3894
vedorofeev <milkofhuk@gmail.com>
2022-09-14 13:22:50 +0700

    Update: Remove FLOAT field type

--- php-package/src/Model/Enums/FieldType.php
+++ php-package/src/Model/Enums/FieldType.php
@@ -9,7 +9,6 @@ enum FieldType: string
 
     case STRING     = 'string';
     case INTEGER    = 'integer';
-    case FLOAT      = 'float';
     case UUID       = 'uuid';
     case DATETIME   = 'datetime';
     case DATE       = 'date';
