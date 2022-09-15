824158a461d025582f703f457565959b29bc54a4
vedorofeev <milkofhuk@gmail.com>
2022-09-15 12:20:54 +0700

    Update: Revert Model Metadata Relations

--- php-package/src/Model/Traits/UsesModelMetadata.php
+++ php-package/src/Model/Traits/UsesModelMetadata.php
@@ -4,15 +4,9 @@ declare(strict_types=1);
 
 namespace Egal\Model\Traits;
 
-use Egal\Model\Builder;
 use Egal\Model\Facades\ModelMetadataManager;
 use Egal\Model\Metadata\FieldMetadata;
 use Egal\Model\Metadata\ModelMetadata;
-use Egal\Model\Metadata\RelationMetadata;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\MorphPivot;
-use Illuminate\Support\Facades\Log;
-use Mockery\Exception;
 
 /**
  * @package Egal\Model
@@ -36,33 +30,6 @@ trait UsesModelMetadata
     public final function getModelMetadata(): ModelMetadata
     {
         return ModelMetadataManager::getModelMetadata(static::class);
-    }
-
-    // TODO: протестировать реализацию
-
-    /**
-     * @return RelationMetadata[]
-     */
-    public function getRelations(): array
-    {
-        return $this->getModelMetadata()->getRelations();
-    }
-
-    /**
-     * Get a specified relationship.
-     *
-     * @param  string  $relationName
-     * @return RelationMetadata
-     */
-    public function getRelation($relationName): mixed
-    {
-        foreach ($this->getRelations() as $relation) {
-            if ($relation->getName() === $relationName) {
-                return $relation;
-            }
-        }
-
-        throw new Exception();
     }
 
     public function getValidationRules(): array
@@ -92,22 +59,6 @@ trait UsesModelMetadata
         }
 
         $this->validationRules[$field->getName()] = $fieldType;
-    }
-
-
-    /**
-     * @param array|string $relations
-     * @return \Illuminate\Database\Eloquent\Builder
-     */
-    public static function with($relations)
-    {
-        $instance = new static();
-        if (is_string($relations)) {
-            $relation = $instance->getRelation($relations);
-            dump($relation->getName());
-            return static::query()->with($relation->getName(), $relation->getClosure());
-        }
-        Log::info('with');
     }
 
 }
