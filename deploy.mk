c4dde4802766ccfaca2f6c90cf8d6ab27469bbea
rollsover <51847757+rollsover@users.noreply.github.com>
2022-10-06 16:22:11 +0700

    Fix build and release scripts.

--- deploy.mk
+++ deploy.mk
@@ -6,7 +6,7 @@ split_only_branch_name := $(split_int_name)-only
 split_tmp_dir_path := $(split_temp_dir_path)/split-$(split_int_name)
 # Script
 split:
-	[[ ! " $(git_branch) " =~ " 2.x 3.x " ]] || ( echo "Error: Only 2.x or 3.x branches supports!" && exit 1 );
+	[[ ! " $(git_branch) " =~ " 2.x 3.x " ]] || ( echo "Error: Only 2.x or 3.x branches supports!" && exit 1 )
 	[[ ! -z "$(split_dir)" ]] || ( echo "Param 'split_dir' must be provided!" && exit 1 )
 	[[ ! -z "$(split_remote)" ]] || ( echo "Param 'split_remote' must be provided!" && exit 1 )
 	# Create branch with
@@ -26,35 +26,9 @@ split:
 	git branch -D $(split_only_branch_name)
 # p.s. Main info from https://stackoverflow.com/questions/359424/detach-move-subdirectory-into-separate-git-repository
 
-npm-publish:
-	[[ ! -z "$(package_dir)" ]] || ( echo "Param 'package_dir' must be provided!" && exit 1 )
-	[[ ! -z "$(version)" ]] || ( echo "Param 'version' must be provided!" && exit 1 )
-	[[ ! -z "$(npm_token)" ]] || ( echo "Param 'npm_token' must be provided!" && exit 1 )
-	cd $(package_dir) && \
-		npm install && \
-		npm version $(version) && \
-		tsc index && \
-		npm run build && \
-		echo "//registry.npmjs.org/:_authToken=$(npm_token)" > .npmrc && \
-		npm publish --access public
-
 deploy@packages/php/framework:
 	make --file=deploy.mk \
 		split_remote=git@github.com:egal/framework-php-package-new.git \
 		split_dir=packages/php/framework \
 		git_branch=$(git_branch) \
 		split
-
-deploy@packages/npm/framework:
-	make --file=deploy.mk \
-		package_dir=packages/npm/framework \
-		version=$(version) \
-		npm_token=$(npm_token) \
-		npm-publish
-
-deploy@packages/npm/widget-library:
-	make --file=deploy.mk \
-		package_dir=packages/npm/widget-library \
-		version=$(version) \
-		npm_token=$(npm_token) \
-		npm-publish
