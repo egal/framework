7a632590b67141c56008e0c638340e8544aa3b85
Stepan <gayvolya@sputnikfund.ru>
2022-10-21 16:02:46 +0700

    Feature: Add hook useActionGetItem() (packages/npm/framework).

--- packages/npm/packages/framework/src/Hooks/Actions/useActionGetItem.tsx
+++ packages/npm/packages/framework/src/Hooks/Actions/useActionGetItem.tsx
@@ -1,18 +1,14 @@
 import { ActionHook, ActionModel, useAction } from './useAction';
 
-type Result<ItemType> = {
-  data: ItemType;
-};
-
 type Params = {
   key: string | number;
   relations?: string[];
 };
 
-export type ActionGetItemHook<ItemType> = ActionHook<Result<ItemType>, Params>;
+export type ActionGetItemHook<ItemType> = ActionHook<ItemType, Params>;
 
 export function useActionGetItem<ItemType>(
   model: ActionModel
 ): ActionGetItemHook<ItemType> {
-  return useAction<Result<ItemType>, Params>(model, 'getItem');
+  return useAction<ItemType, Params>(model, 'getItem');
 }
