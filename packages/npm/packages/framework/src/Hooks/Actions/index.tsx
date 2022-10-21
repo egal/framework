2b1348c196b5ec3e00fc05165ad25e9bc463aafe
Neirokin <56441777+Neirokin@users.noreply.github.com>
2022-10-21 16:05:56 +0700

    Feature: Add hook useActionGetItem() (packages/npm/framework). (#59)
    * [EGAL-CLIENT] Actions. Action getItem added
    
    * Feature: Add hook useActionGetItem() (packages/npm/framework).
    
    Co-authored-by: Stepan <gayvolya@sputnikfund.ru>

--- packages/npm/packages/framework/src/Hooks/Actions/index.tsx
+++ packages/npm/packages/framework/src/Hooks/Actions/index.tsx
@@ -1,6 +1,7 @@
 export * from './useAction';
 export * from './useActionCreate';
 export * from './useActionDelete';
+export * from './useActionGetItem';
 export * from './useActionGetItems';
 export * from './useActionGetMetadata';
 export * from './useActionUpdate';
