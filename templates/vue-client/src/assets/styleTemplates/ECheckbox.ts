1b44edf74a1f3f41a58da6754eebf7b3ba34107b
rollsover <51847757+rollsover@users.noreply.github.com>
2022-10-25 12:48:59 +0700

    Update: Refactoring release, auth-service as template and other little things.

--- templates/vue-client/src/assets/styleTemplates/ECheckbox.ts
+++
@@ -1,8 +0,0 @@
-export const checkboxStyleConfig = {
-  borderColor: null,
-  backgroundColor: null,
-  hover: { borderColor: null, backgroundColor: null },
-  active: { borderColor: null, backgroundColor: null },
-  checked: { borderColor: null, backgroundColor: null },
-  disabled: { borderColor: null, backgroundColor: null },
-}
