f37c6005b4fb9f9bf6c1c1049415b460353fa850
rollsover <51847757+rollsover@users.noreply.github.com>
2022-10-26 11:20:55 +0700

    Fix: Remove Model.isInstanceForAction status and relevant events and algorithms, because it not needs while using Model Policies (packages/php/framework).

--- packages/php/src/Model/Traits/HasEvents.php
+++ packages/php/src/Model/Traits/HasEvents.php
@@ -12,57 +12,7 @@ use Egal\Core\Events\GlobalEvent;
 trait HasEvents
 {
 
-    public static function retrievedWithAction($callback)
-    {
-        static::registerModelEvent('retrieved.action', $callback);
-    }
-
-    public static function creatingWithAction($callback)
-    {
-        static::registerModelEvent('creating.action', $callback);
-    }
-
-    public static function createdWithAction($callback)
-    {
-        static::registerModelEvent('created.action', $callback);
-    }
-
-    public static function updatingWithAction($callback)
-    {
-        static::registerModelEvent('updating.action', $callback);
-    }
-
-    public static function updatedWithAction($callback)
-    {
-        static::registerModelEvent('updated.action', $callback);
-    }
-
-    public static function savingWithAction($callback)
-    {
-        static::registerModelEvent('saving.action', $callback);
-    }
-
-    public static function savedWithAction($callback)
-    {
-        static::registerModelEvent('saved.action', $callback);
-    }
-
-    public static function deletingWithAction($callback)
-    {
-        static::registerModelEvent('deleting.action', $callback);
-    }
-
-    public static function deletedWithAction($callback)
-    {
-        static::registerModelEvent('deleted.action', $callback);
-    }
-
-    /**
-     * @param string $event
-     * @param string $method
-     * @return mixed|null
-     */
-    protected function fireCustomModelEvent($event, $method)
+    protected function fireCustomModelEvent(string $event, string $method): mixed
     {
         $result = parent::fireCustomModelEvent($event, $method);
 
@@ -73,20 +23,6 @@ trait HasEvents
         }
 
         return $result;
-    }
-
-    protected function fireModelEvent($event, $halt = true)
-    {
-        if ($this->isInstanceForAction) {
-            $this->fireActionEvent($event, $halt);
-        }
-
-        parent::fireModelEvent($event, $halt);
-    }
-
-    protected function fireActionEvent($event, $halt = true)
-    {
-        parent::fireModelEvent($event . '.action', $halt);
     }
 
 }
