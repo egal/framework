15d48122b44fea41b40cda9e7542c856108bf204
lebedev@sputnikfund.ru <lebedev@sputnikfund.ru>
2022-10-31 13:39:11 +0700

    update_notifications

---
+++ stand/server/notification-service/database/migrations/2022_31_10_100001_create_broadcast_message_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('broadcast_message', function (Blueprint $table) {
+            $table->id();
+            $table->timestamp('starts_at');
+            $table->timestamp('ends_at');
+            $table->string('background_color');
+            $table->string('message');
+            $table->boolean('active');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('broadcast_message');
+    }
+};
