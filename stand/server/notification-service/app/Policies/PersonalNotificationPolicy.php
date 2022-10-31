5d6d5d664c3ea5da2eef12b7e2c8d1ed802482c6
lebedev@sputnikfund.ru <lebedev@sputnikfund.ru>
2022-10-31 18:17:28 +0700

    update_notifications

---
+++ stand/server/notification-service/app/Policies/PersonalNotificationPolicy.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\PersonalNotification;
+use Egal\Auth\Entities\Client;
+
+class PersonalNotificationPolicy
+{
+    public static function retrieving(Client $client, PersonalNotification $notification)
+    {
+        return $client->getSub()['id'] === $notification->getAttribute('user_id');
+    }
+
+    public static function retrieved(Client $client, PersonalNotification $notification)
+    {
+
+        return true;
+    }
+
+    public static function retrievingMetadata(Client $client, PersonalNotification $notification)
+    {
+        return $client->getSub()['id'] === $notification->getAttribute('user_id');
+    }
+
+    public static function retrievedMetadata(Client $client, PersonalNotification $notification)
+    {
+        return true;
+    }
+
+    public static function retrievingCount(Client $client, PersonalNotification $notification)
+    {
+        return $client->getSub()['id'] === $notification->getAttribute('user_id');
+
+    }
+
+    public static function retrievedCount(Client $client, PersonalNotification $notification)
+    {
+        return true;
+    }
+
+    public static function creating(Client $client, PersonalNotification $notification)
+    {
+        return $client->isService();
+    }
+
+    public static function created(Client $client, PersonalNotification $notification)
+    {
+        return true;
+    }
+
+    public static function updating(Client $client, PersonalNotification $notification)
+    {
+        return $client->isService() || $client->isUser() && $notification->getDirty() === ['checked'];
+    }
+
+    public static function updated(Client $client, PersonalNotification $notification)
+    {
+        return true;
+    }
+
+    public static function deleting(Client $client, PersonalNotification $notification)
+    {
+        return false;
+    }
+
+    public static function deleted(Client $client, PersonalNotification $notification)
+    {
+        return false;
+    }
+}
--- stand/server/notification-service/database/migrations/2022_31_10_100001_create_broadcast_message_table.php
+++ stand/server/notification-service/database/migrations/2022_31_10_100001_create_broadcast_message_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('broadcast_messages', function (Blueprint $table) {
-            $table->id();
+            $table->uuid('id')->primary();
             $table->timestamp('starts_at');
             $table->timestamp('ends_at');
             $table->string('background_color');
